11e24a2d6f8728c22ad3f4149b00681942a727ac
Thomas Garry <henbit1234@gmail.com>
2026-08-17 19:27:39 -0500

    Add OCTO Android Camera2 research platform

---
+++ app/src/main/java/com/example/hi_snr_computational_imaging/writeImagingScriptHere.kt
@@ -0,0 +1,24 @@
+package com.example.hi_snr_computational_imaging
+
+import android.content.Context
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CaptureRequest
+import android.hardware.camera2.DngCreator
+import android.hardware.camera2.TotalCaptureResult
+import android.media.ImageReader
+import android.util.Log
+import java.io.FileOutputStream
+import java.io.File
+
+
+//A place for a custom imaging script to be specified using methods from CameraMethods.kt
+fun customImagingScript(camera2Controller: Camera2Controller){
+
+    camera2Controller.setISO(500)
+    camera2Controller.saveRaw("test")
+
+
+}
+
